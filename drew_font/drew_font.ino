d09d0ac5b5265569b2205fbe4f53f5cfa8b8f62c
Simon Prickett <simon@moduscreate.com>
2016-11-28 09:29:33 -0800

    Fixes letter pointers.

--- drew_font/drew_font.ino
+++ drew_font/drew_font.ino
@@ -2,6 +2,7 @@
 #include "letters.h"
 Arduboy arduboy;
 
+char * alphabet[26];
 
 void drawBitmap(byte x, byte y, const uint8_t *bitmap, uint8_t frame) {
   unsigned int frame_offset;
@@ -23,6 +24,32 @@ void setup() {
   arduboy.initRandomSeed();
   arduboy.beginNoLogo();
 
+  alphabet[0] = A;
+  alphabet[1] = B;
+  alphabet[2] = C;
+  alphabet[3] = D;
+  alphabet[4] = E;
+  alphabet[5] = F;
+  alphabet[6] = G;
+  alphabet[7] = H;
+  alphabet[8] = I;
+  alphabet[9] = J;
+  alphabet[10] = K;
+  alphabet[11] = L;
+  alphabet[12] = M;
+  alphabet[13] = N;
+  alphabet[14] = O;
+  alphabet[15] = P;
+  alphabet[16] = Q;
+  alphabet[17] = R;
+  alphabet[18] = S;
+  alphabet[19] = T;
+  alphabet[20] = U;
+  alphabet[21] = V;
+  alphabet[22] = W;
+  alphabet[23] = X;
+  alphabet[24] = Y;
+  alphabet[25] = Z;
 }
 
 void loop() {
@@ -34,13 +61,6 @@ void loop() {
        y = random(5, 60);
 
   arduboy.clear();
-//  for (byte r = 1; r < 12; r++) {
-//
-//      arduboy.fillCircle(x, y, r, 1);
-//     
-////      delay(10);
-//  }
-
   
   for (byte i = 0; i < 26; i++) {
     byte xPos = (6 * i) + 2,
@@ -52,11 +72,8 @@ void loop() {
      
     }
 
-//    unsigned char* letter = alphabet[i];
-    drawBitmap(xPos,yPos, alphabet[0], 0);
-
+    drawBitmap(xPos,yPos, alphabet[i], 0);
   }
 
   arduboy.display();
-
 }
